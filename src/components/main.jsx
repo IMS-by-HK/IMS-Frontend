60c26a2f8abfb7ab2fc0c018718a3f9b7c9a138c
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-12-11 14:51:43 +1000

    Started the main page table front-end

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,20 +3,45 @@ import '../styles/main.css';
 function Main() {
     return (
         <div className="inventory">
-          <h1 className="title">Inventory Management System</h1>
+            {/* Main Page Title */}
+            <h1 className="title">Inventory Management System</h1>
 
-          <div className="search">
-            <input
-              type="text"
-              className="input"
-              placeholder="Search product name..."
-            />
-            <button className="button filter">Filter</button>
-          </div>
+            {/* Container for Input and Filter Button */}
+            <div className="search">
+                {/* Input Field */}
+                <input
+                    type="text"
+                    className="input"
+                    placeholder="Search product name..."
+                />
+                {/* Filter Button */}
+                <button className="button filter">Filter</button>
+            </div>
 
+            {/* NEED: Container for Table, Header Container for Item Details, 2 Containers side-by-side under header */}
+
+            <div className="mainContainer">
+
+                {/* Header Container containing Add Item and Item Categories  */} 
+                <div className="headerContainer">
+
+                    {/* Add Item */}
+                    <div className="headerLeft"></div>
+                    {/* Item Categories */}
+                    <div className="headerRight"></div>
+
+                </div>
+
+                {/* Contains the Items and Item Specifics */}
+                <div className="tableContainer">
+                    <div className="itemName"></div>
+                    <div className="itemSpecifics"></div>
+                </div>
+
+            </div>
 
         </div>
-      );
-    };
+    );
+};
 
 export default Main;
