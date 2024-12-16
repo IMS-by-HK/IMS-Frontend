11839656e2028b5f379e455e2e764ab90082b9e4
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-12-16 16:06:30 +1000

    Added login functinality

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,32 +1,58 @@
-import '../styles/login.css';
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import "../styles/login.css";
 
 function Login() {
-    return (
-        <div className="login">
-          <p className="titleHeading">INVENTORY MANAGEMENT SYSTEM</p>
-          <div className="form">
-            <label className="label" htmlFor="username">
-              USERNAME
-            </label>
-            <input
-              className="input"
-              type="text"
-              id="username"
-              placeholder="ADMIN"
-            />
-            <label className="label" htmlFor="password">
-              PASSWORD
-            </label>
-            <input
-              className="input"
-              type="password"
-              id="password"
-              placeholder="ADMIN"
-            />
-            <button className="button">Login</button>
-          </div>
-        </div>
-      );
-    };
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/login', formData);
+      if (response.status === 200) {
+        navigate('/');
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  };
+
+  return (
+    <div className="login">
+      <p className="titleHeading">INVENTORY MANAGEMENT SYSTEM</p>
+      <form onSubmit={handleSubmit} className="form">
+        <label className="label" htmlFor="username">USERNAME</label>
+        <input 
+          className="input" 
+          type="text" 
+          id="username" 
+          onChange={handleChange} 
+          placeholder="ADMIN" 
+        />
+        <label className="label" htmlFor="password">PASSWORD</label>
+        <input 
+          className="input" 
+          type="password" 
+          id="password" 
+          onChange={handleChange} 
+          placeholder="ADMIN" 
+        />
+        <button className="button" type="submit">LOGIN</button>
+      </form>
+    </div>
+  );
+}
 
 export default Login;
